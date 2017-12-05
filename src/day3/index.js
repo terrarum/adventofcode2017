cfd31033e3d4bda72f14945a929dd89273bbc757
James Fidler <james.fidler@immediate.co.uk>
2017-12-05 10:01:58 +0000

    I think I have the mapping right

--- src/day3/index.js
+++ src/day3/index.js
@@ -1,5 +1,26 @@
 const getCellPos = function getCellPos(cellId) {
+  let leg = 1; // count
+  let legLength = 1; // length
+  let legEnd = 2; // cellId
+  let x = 0;
+  let y = 0;
 
+  // Loop through each cell.
+  for (let cell = 1; cell <= cellId; cell++) {
+    // Identify the last cell of the leg.
+    if (cell === legEnd) {
+      // Update the current leg.
+      leg += 1;
+      // Update the length of the current leg if this is an odd leg.
+      if (leg % 2) {
+        legLength = (leg + 1) / 2;
+      }
+      // Set the new leg end.
+      legEnd = cell + legLength;
+    }
+
+    console.log(`Cell:  ${cell}\tLeg: ${leg}\tLength: ${legLength}\tLeg End: ${legEnd}`);
+  }
 };
 
 const taskOne = function taskOne(cellId) {
@@ -14,3 +35,5 @@ module.exports = {
   taskOne,
   taskTwo,
 };
+
+taskOne(12);
