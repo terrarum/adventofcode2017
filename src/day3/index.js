1447b1817f8f6eb719259aff3c0d3f6b27efdcfa
James Fidler <james.fidler@immediate.co.uk>
2017-12-06 15:03:17 +0000

    Returning correct coordinate.

--- src/day3/index.js
+++ src/day3/index.js
@@ -1,30 +1,69 @@
+const isOdd = function isOdd(num) {
+  return num % 2;
+};
+
+const isEven = function isEven(num) {
+  return !isOdd(num);
+};
+
+const getVector = function getVector(leg, legLength) {
+  if (isEven(leg) && isOdd(legLength)) {
+    return { x: 0, y: 1}
+  }
+  if (isOdd(leg) && isEven(legLength)) {
+    return { x: -1, y: 0}
+  }
+  if (isOdd(leg) && isOdd(legLength)) {
+    return { x: 1, y: 0}
+  }
+  if (isEven(leg) && isEven(legLength)) {
+    return { x: 0, y: -1}
+  }
+};
+
 const getCellPos = function getCellPos(cellId) {
   let leg = 1; // count
   let legLength = 1; // length
   let legEnd = 2; // cellId
-  let x = 0;
-  let y = 0;
 
-  // Loop through each cell.
-  for (let cell = 1; cell <= cellId; cell++) {
+  let currentCell = {
+    x: 0,
+    y: 0
+  };
+
+  let vector = {
+    x: 1,
+    y: 0,
+  };
+
+  // Loop through each cell id.
+  for (let cell = 1; cell < cellId; cell++) {
+
     // Identify the last cell of the leg.
     if (cell === legEnd) {
       // Update the current leg.
       leg += 1;
       // Update the length of the current leg if this is an odd leg.
-      if (leg % 2) {
+      if (isOdd(leg)) {
         legLength = (leg + 1) / 2;
       }
       // Set the new leg end.
       legEnd = cell + legLength;
+      // Get vector.
+      vector = getVector(leg, legLength);
     }
+    currentCell.x += vector.x;
+    currentCell.y += vector.y;
 
-    console.log(`Cell:  ${cell}\tLeg: ${leg}\tLength: ${legLength}\tLeg End: ${legEnd}`);
+    if (cell === cellId - 1) {
+      return currentCell;
+    }
   }
 };
 
 const taskOne = function taskOne(cellId) {
   const cellPos = getCellPos(cellId);
+  console.log(cellPos);
 };
 
 const taskTwo = function taskTwo(input) {
@@ -37,3 +76,4 @@ module.exports = {
 };
 
 taskOne(12);
+taskOne(23);
