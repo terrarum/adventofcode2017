9eb9122a0b23e639a92b337ef732bdbd4bf96e96
James Fidler <terrarum@gmail.com>
2017-12-11 23:53:52 +0000

    fixed task one again, ffs

--- src/day3/index.js
+++ src/day3/index.js
@@ -2,49 +2,33 @@ const isOdd = function isOdd(num) {
   return num % 2;
 };
 
-const isEven = function isEven(num) {
-  return !isOdd(num);
-};
-
 const pad = function pad(num) {
   return num < 10 ? `0${num}` : num;
 };
 
-const getVector = function getVector(leg, legLength) {
-  if (isEven(leg) && isOdd(legLength)) {
+const getVector = function getVector(dir) {
+  if (dir === 0) { // right
     return { x: 1, y: 0}
   }
-  if (isOdd(leg) && isEven(legLength)) {
-    return { x: -1, y: 0}
-  }
-  if (isOdd(leg) && isOdd(legLength)) {
+  if (dir === 1) { // up
     return { x: 0, y: 1}
   }
-  if (isEven(leg) && isEven(legLength)) {
+  if (dir === 2) { // left
+    return { x: -1, y: 0}
+  }
+  if (dir === 3) { // down
     return { x: 0, y: -1}
   }
-};
-
-const expected = {
-  1: 1,
-  2: 1,
-  3: 2,
-  4: 4,
-  5: 5,
-  6: 10,
-  7: 11,
-  8: 23,
-  9: 25,
-  10: 26,
 };
 
 const getCellPos = function getCellPos(cellId) {
   let leg = 1; // count
   let legLength = 1; // length
   let legEnd = 2; // cellId
+  let dir = 0;
 
   let currentCell = {
-    x: 0,
+    x: 1,
     y: 0
   };
 
@@ -53,19 +37,25 @@ const getCellPos = function getCellPos(cellId) {
     y: 0,
   };
 
-  for (let cell = 1; cell < cellId + 1; cell++) {
+  if (cellId === 1) {
+    return {x: 0, y: 0};
+  }
+
+  for (let cell = 1; cell < cellId; cell++) {
     if (cell === legEnd) {
       leg += 1;
+      dir += 1;
+      dir = dir > 3 ? 0 : dir;
       if (isOdd(leg)) {
         legLength = (leg + 1) / 2;
       }
       legEnd = cell + legLength;
-      vector = getVector(leg, legLength);
+      vector = getVector(dir);
     }
 
     currentCell.x += vector.x;
     currentCell.y += vector.y;
-    console.log(`cellId: ${cellId}\tx: ${currentCell.x}\ty: ${currentCell.y}\tleg: ${leg}\tlegLength: ${legLength}\tdist: ${calculateManhattan(currentCell)}`);
+    console.log(cell, dir, vector, currentCell);
   }
 
   return currentCell;
@@ -80,6 +70,10 @@ const taskOne = function taskOne(cellId) {
   const manhattan = calculateManhattan(cellPos);
 
   return manhattan;
+};
+
+const sumSurrounding = function sumSurrounding(pos) {
+
 };
 
 const taskTwo = function taskTwo(cellId) {
