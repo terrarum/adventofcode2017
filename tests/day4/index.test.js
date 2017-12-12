ce52e31b7c3b4a4f0b1c2eddf4134b26f8dd7881
James Fidler <terrarum@gmail.com>
2017-12-12 22:17:10 +0000

    Day Four Task Two tests

--- tests/day4/index.test.js
+++ tests/day4/index.test.js
@@ -7,5 +7,9 @@ test('Day Four Task One', () => {
 });
 
 test('Day Four Task Two', () => {
-
+  expect(day4.processAnagramPassphrase('abcde fghij')).toBe(true);
+  expect(day4.processAnagramPassphrase('abcde xyz ecdab')).toBe(false);
+  expect(day4.processAnagramPassphrase('a ab abc abd abf abj')).toBe(true);
+  expect(day4.processAnagramPassphrase('iiii oiii ooii oooi oooo')).toBe(true);
+  expect(day4.processAnagramPassphrase('oiii ioii iioi iiio')).toBe(false);
 });
