6132404a90daaeb5626ac332a88e3ba0f25399e7
James Fidler <james.fidler@immediate.co.uk>
2017-12-15 17:22:48 +0000

    Recursion

--- src/day5/index.js
+++ src/day5/index.js
@@ -1,11 +1,21 @@
-const processInstructions = function processInstructions(instructions) {
-
-  return 0;
+const processInstructions = function processInstructions(instructions, steps, currentStep) {
+  console.log(instructions[currentStep]);
+  if (instructions[currentStep] !== undefined) {
+    currentStep++;
+    return processInstructions(instructions, steps, currentStep);
+  }
+  else {
+    console.log('a', currentStep);
+    return currentStep;
+  }
 };
 
 const taskOne = function taskOne(instructionString) {
   const instructions = instructionString.split('\n');
-  const steps = processInstructions(instructions);
+
+  let steps = 0;
+  let currentStep = 0;
+  steps = processInstructions(instructions, steps, currentStep);
   return steps;
 };
 
